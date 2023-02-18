f8997018110f54aec5531b8c442aeb2ff172bc55
Sai Kiran Chitti <chittis2019@gmail.com>
2023-02-18 02:42:14 -0800

    added comments

--- print.py
+++ print.py
@@ -1,9 +1,9 @@
 print("happy coding and github testing")
 
 # checking git pull condition
-i = i
-for i in range(10):
-    print(i+=2)
-print(i)
+# i = i
+# for i in range(10):
+#     print(i+=2)
+# print(i)
 
 # need to check cherry pick and resolve conflicts next.
