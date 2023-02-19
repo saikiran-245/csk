9c74a1b4b229554d8642ecece8ab44235aa5b9a7
Sai Kiran Chitti <chittis2019@gmail.com>
2023-02-19 00:27:02 -0800

    Feb 18 learnings

---
+++ python/P_feb_18.py
@@ -0,0 +1,67 @@
+#first 
+
+expense_list = [2200, 2350, 2600, 2130, 2190]
+
+# val = expense_list[1]-expense_list[0]
+
+# print("Dollars spend extra " ,val )
+
+# print("total expense in first quarter = ", expense_list[0]+ expense_list[1] + expense_list[2])
+
+# for i in expense_list:
+#     if i == 2000:
+#         print("expesen spent is equal to 2000")
+# else:
+#     print("No expense matched")
+
+# expense_list.append(1980)
+# print(expense_list)
+
+# april_exp = expense_list[3]
+
+# refund = 200
+
+# expense_list[3] = april_exp - refund
+
+# print(expense_list)
+
+#2
+
+heros=['spider man','thor','hulk','iron man','captain america']
+
+# # print(len(heros))
+
+# heros.append('black panther')
+
+# # print(heros)
+
+# heros.pop(5)
+
+# # print(heros)
+
+# heros[3] = "black panther"
+
+
+# # print(heros)
+
+heros[1:3] = ['doctor strange'] 
+
+print(heros)
+
+# heros.sort()
+
+# print(heros)
+
+#3
+
+# li = []
+
+# max_number = int(input("Enter a number = "))
+
+# for i in range(max_number):
+#     if i%2 !=0:
+#         li.append(i)
+# print(li)
+
+
+
